b46253a5b57c9002f31740d6c04089a7d294776b
anuragjha <toanuragjha@gmail.com>
2020-08-17 03:38:50 -0700

    websocket connection to a kakfa consumer in main.py

---
+++ main.py
@@ -0,0 +1,180 @@
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+
+import asyncio
+import json
+import typing
+
+from aiokafka import AIOKafkaConsumer
+# from app.core.config import KAFKA_INSTANCE
+# from app.core.config import PROJECT_NAME
+# from app.core.models.model import ConsumerResponse
+from fastapi import FastAPI
+from fastapi import WebSocket
+from loguru import logger
+from starlette.endpoints import WebSocketEndpoint
+from starlette.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+from pydantic import confloat
+from pydantic import StrictStr
+
+KAFKA_INSTANCE = "localhost:9092"
+PROJECT_NAME = "tester"
+app = FastAPI(title=PROJECT_NAME)
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        print("send_personal_message: "+ message)
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        print("broadcast:", message)
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+@app.get("/connections")
+async def get():
+    return len(manager.active_connections)
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
+################
+################
+async def consume(consumer, topicname):
+    async for msg in consumer:
+        print("for msg in consumer: ", msg)
+        return msg.value.decode()
+
+
+@app.websocket_route("/consumer/{topicname}")
+class WebsocketConsumer(WebSocketEndpoint):
+    """
+    Consume messages from <topicname>
+    This will start a Kafka Consumer from a topic
+    And this path operation will:
+    * return ConsumerResponse
+    """
+
+    async def on_connect(self, websocket: WebSocket) -> None:
+        topicname = websocket["path"].split("/")[2]  # until I figure out an alternative
+
+        await websocket.accept()
+        await websocket.send_json({"Message: ": "connected"})
+
+        loop = asyncio.get_event_loop()
+        self.consumer = AIOKafkaConsumer(
+            topicname,
+            loop=loop,
+            client_id=PROJECT_NAME,
+            bootstrap_servers=KAFKA_INSTANCE,
+            enable_auto_commit=False,
+        )
+
+        await self.consumer.start()
+
+        self.consumer_task = asyncio.create_task(
+            self.send_consumer_message(websocket=websocket, topicname=topicname)
+        )
+
+        logger.info("connected")
+
+    async def on_disconnect(self, websocket: WebSocket, close_code: int) -> None:
+        self.consumer_task.cancel()
+        await self.consumer.stop()
+        logger.info(f"counter: {self.counter}")
+        logger.info("disconnected")
+        logger.info("consumer stopped")
+
+    async def on_receive(self, websocket: WebSocket, data: typing.Any) -> None:
+        await websocket.send_json({"Message: ": data})
+
+    async def send_consumer_message(self, websocket: WebSocket, topicname: str) -> None:
+        self.counter = 0
+        while True:
+            data = await consume(self.consumer, topicname)
+            print("data : ", data)
+            # response = ConsumerResponse(topic=topicname, **json.loads(data))
+            logger.info(data)#response)
+            # await websocket.send_text(f"{response.json()}")
+            await websocket.send_text(f"{data}")
+            print("websocket.send_text done")
+            self.counter = self.counter + 1
+
+
+@app.get("/ping")
+def ping():
+    return {"ping": "pong!"}
+
+########################
+########################
