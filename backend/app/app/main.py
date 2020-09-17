4c3de4f231f9767e2021db83368a0b03274163cd
anuragjha <toanuragjha@gmail.com>
2020-09-16 17:13:51 -0700

    Added kafka schema registry and kafka control center images

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -4,19 +4,9 @@ from starlette.middleware.cors import CORSMiddleware
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 from app.core.logger_middleware import LoggerMiddleware
-from app.core.second_middleware import SecondMiddleware
 
 from app.core.logger_config import LoggerConfig
 
-# from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
-#
-#
-# elasticapm = make_apm_client({
-#     'SERVICE_NAME': 'dex-api-main',
-#     'SECRET_TOKEN': 'secret-token-apm-client',
-#     'SERVER_URL': 'http://localhost:5000'
-# })
-# #============
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json",
@@ -34,8 +24,6 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-# app.add_middleware(ElasticAPM, client=elasticapm)
 app.add_middleware(LoggerMiddleware)  # todo: introduces =>   INFO:     ASGI 'lifespan' protocol appears unsupported.
-# app.add_middleware(SecondMiddleware)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
