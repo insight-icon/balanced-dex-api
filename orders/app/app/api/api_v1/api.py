35d5a295a50f1c5f9df3d3a9958690f6f8678c18
anuragjha <toanuragjha@gmail.com>
2020-10-17 02:48:59 -0700

    refactoring apis

--- orders/app/app/api/api_v1/api.py
+++ orders/app/app/api/api_v1/api.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import dex, eventer, crud, balanced, depth, kline, transaction
+from app.api.api_v1.endpoints import balanced, depth, kline, transaction
 
 api_router = APIRouter()
-api_router.include_router(dex.router, prefix="/dex",   tags=["dex"])
-api_router.include_router(eventer.router, prefix="/eventer",   tags=["eventer"])
-api_router.include_router(crud.router, prefix="/crud", tags=["crud"])
 
 api_router.include_router(balanced.router, prefix="/balanced", tags=["balanced"])
 api_router.include_router(depth.router, prefix="/balanced/depth", tags=["depth"])
